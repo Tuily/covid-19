51a2041c7ae20bf4fb11f66c705005187574a069
tuily <brizolla.tuily@gmail.com>
2020-04-02 13:29:35 -0300

    Updates

---
+++ precache-manifest.ca86c3b6807cd85844103095b8043215.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06ba65612e06c21ee23ab6095a8b5276",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "6425a95d159e013a9b0f",
+    "url": "/covid-19/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "3f7b7497947ef832307f",
+    "url": "/covid-19/static/css/main.1a7a836a.chunk.css"
+  },
+  {
+    "revision": "6425a95d159e013a9b0f",
+    "url": "/covid-19/static/js/2.6573bf3f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid-19/static/js/2.6573bf3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f7b7497947ef832307f",
+    "url": "/covid-19/static/js/main.336dc86c.chunk.js"
+  },
+  {
+    "revision": "8a52c8d37d83ffe3717e",
+    "url": "/covid-19/static/js/runtime-main.1a3018c3.js"
+  }
+]);
